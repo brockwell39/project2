8596f5efdddb8430b96102423a8b949006673c89
Alex Catlow <alexcatlow@Alexs-MBP.home>
2020-05-12 17:15:07 +0100

    set up the style and js sheets and got the first part of check going

--- application.py
+++ application.py
@@ -1,14 +1,21 @@
 import os
+import requests
 
-from flask import Flask
+from flask import Flask,render_template,jsonify,request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+users = []
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
-print(test)
+    return render_template("index.html")
+
+@app.route("/check", methods=["POST"])
+def check():
+    user = request.form.get("user")
+    print(user)
+    return jsonify({"success":True})
