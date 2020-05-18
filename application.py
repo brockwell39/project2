58fc8cf4f9d44706e387df32674da0373f79e36d
Alex Catlow <alexcatlow@Alexs-MBP.home>
2020-05-18 16:53:30 +0100

    got the socket io up and running now need to attach and dsiplay the data to the chat

--- application.py
+++ application.py
@@ -9,41 +9,50 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 users = []
-channels =[]
+channelsi =[]
+messages =["cat"]
 
-
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def index():
-    return render_template("index.html")
+        return render_template("chat.html",channelsi=channelsi)
+
+
+
+@app.route("/newuser", methods=["POST","GET"])
+def newuser():
+        # Check in case JavaScript is disabled or fails
+        if request.method == "POST":
+            user = request.form.get("user")
+            if user in users:
+                error = "That username is taken"
+                return render_template("error.html", error=error)
+            elif user == "":
+                error = "Please enter a username"
+                return render_template("error.html", error=error)
+            else:
+                users.append(user)
+                return redirect("/chat")
+
+        return render_template("index.html")
 
 @app.route("/chat", methods=["POST","GET"])
 def chat():
-    # Check in case JavaScript is disabled or fails
-    if request.method == "POST":
-        user = request.form.get("user")
-        if user in users:
-            error = "That username is taken"
-            return render_template("error.html", error=error)
-        elif user == "":
-            error = "Please enter a username"
-            return render_template("error.html", error=error)
-        else:
-            users.append(user)
+    print(channelsi)
+    return render_template("chat.html",channelsi=channelsi)
 
-    return render_template("chat.html", user=user)
-
-@app.route("/channels", methods=["POST","GET"])
-def channels():
+@app.route("/channels/<string:channel_id>", methods=["POST","GET"])
+def channels(channel_id):
     # Check in case JavaScript is disabled or fails
-    return render_template("/channels.html")
+    return render_template("/channels.html",channelsi=channelsi)
 
 @app.route("/channelcheck", methods=["POST","GET"])
 def channelcheck():
     print("test")
     channel = request.form.get("channel")
-    if channel in channels:
+    if channel in channelsi:
         return jsonify({"success":False})
     else:
+        channelsi.append(channel)
         return jsonify({"success":True})
 
 
@@ -55,3 +64,13 @@ def check():
         return jsonify({"success":False})
     else:
         return jsonify({"success":True})
+
+@socketio.on("chat")
+def vote(data):
+    message = data["message"]
+    user = data["user"]
+    message = message + "from" + user
+    messages.append(message)
+    print("mes",messages)
+    print(user)
+    emit("announce message", messages , broadcast=True)
--- static/index.js
+++ static/index.js
@@ -1,9 +1,20 @@
-document.addEventListener("DOMContentLoaded",function(){
-  document.getElementById("bbutton").addEventListener("click",function(event){
-    event.preventDefault()
+
+if(!(localStorage.getItem('user'))){
+    window.location.href = "http://127.0.0.1:5000/newuser";
+  }
+if(localStorage.getItem('user')){
+      user = localStorage.getItem('user');
+      document.getElementById('user').innerHTML = user;
+    }
+
+
+
+
+
+function func1(){
     const request = new XMLHttpRequest;
     const user = document.querySelector("#user").value;
-
+    if(user != ''){
     request.open('POST','/check');
 
     request.onload = function(){
@@ -12,6 +23,7 @@ document.addEventListener("DOMContentLoaded",function(){
 
       if(data.success){
         document.getElementById('index').submit();
+        localStorage.setItem('user',user);
       }
       else{
         document.getElementById('index').reset();
@@ -23,34 +35,58 @@ document.addEventListener("DOMContentLoaded",function(){
 
     request.send(data);
     return false;
-  });
-});
+   }
+   else{
+     document.getElementById('index').reset();
+     alert("Please enter a username")
+   };
+  };
 
-function myfunc(){
-  alert("test")
-  document.getElementById("channel").addEventListener("click",function(event){
-    event.preventDefault()
-    const request = new XMLHttpRequest;
-    const user = document.querySelector("#user").value;
+function func2(){
+      const request = new XMLHttpRequest;
+      const channel = document.querySelector("#channel").value;
+      if(channel != ''){
+      request.open('POST','/channelcheck');
 
-    request.open('POST','/channelcheck');
+      request.onload = function(){
 
-    request.onload = function(){
+        const data = JSON.parse(request.responseText);
 
-      const data = JSON.parse(request.responseText);
-
-      if(data.success){
-        document.getElementById('channel').submit();
+        if(data.success){
+          document.getElementById('channelf').submit();
+        }
+        else{
+          document.getElementById('channelf').reset();
+          alert("That channel name is in use");
+        }
       }
-      else{
-        document.getElementById('channel').reset();
-        alert("That channel is in use");
-      }
-    }
-    const data = new FormData();
-    data.append('user',user);
+      const data = new FormData();
+      data.append('channel',channel);
 
-    request.send(data);
-    return false;
-  });
+      request.send(data);
+      return false;
+     }
+     else{
+       document.getElementById('channelf').reset();
+       alert("Please enter a channel name")
+     };
+    };
+
+var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+function chat1(){
+  var user = localStorage.getItem('user');
+  socket.on('connect');
+  const message = document.getElementById('message').value;
+  socket.emit('chat', {'message': message,'user':user});
+  document.getElementById('chatf').reset();
 };
+
+socket.on('announce message',function(data){
+  document.querySelector('#Textarea1').innerHTML='';
+  var i;
+for (i = 0; i < data.length; i++) {
+  let li = data[i] + "\n";
+
+  document.querySelector('#Textarea1').append(li);
+  };
+});
